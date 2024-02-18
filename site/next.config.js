91cb3d5f109b66aa7896102ae9f2fc146f4f193f
Michael Napodano <michaelpnap@gmail.com>
2024-02-18 00:30:53 -0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Bingus Workspaces',
+    description: 'My own collection of custom workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
